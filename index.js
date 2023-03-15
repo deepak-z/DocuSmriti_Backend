e9cdd11e93be0cd90bad5372786a2b173573085b
Deepak Kumar <deepak.kumar@zebpay.com>
2023-03-15 14:15:01 +0530

    AddingHostAndFormatting

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const port = 3333;
-var nodemailer = require('nodemailer');
-require('dotenv').config();
+var nodemailer = require("nodemailer");
+require("dotenv").config();
 
 var transporter = nodemailer.createTransport({
-    service: process.env.sendingEmailService,
-    auth: {
-      user: process.env.sendingEmail,
-      pass: process.env.sendingEmailPassword,
-    }
-  });
+  service: process.env.sendingEmailService,
+  auth: {
+    user: process.env.sendingEmail,
+    pass: process.env.sendingEmailPassword,
+  },
+});
 
-app.post('/sendMail', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("This is Docusmriti Backend");
+});
 
-    console.log(req.body.receiverEmail);
-    console.log(req.body.subject);
-    console.log(process.env.sendingEmail);
-    
+app.post("/sendMail", (req, res) => {
+  console.log(req.body.receiverEmail);
+  console.log(req.body.subject);
+  console.log(process.env.sendingEmail);
 
   var mailOptions = {
-    from: process.env.sendingEmail, 
+    from: process.env.sendingEmail,
     to: req.body.receiverEmail,
     subject: req.body.subject,
-    text: 'Flex marta hain saala'
+    text: "Flex marta hain saala",
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log("Email sent: " + info.response);
       res.send("Email sent successfully");
     }
   });
-  
-
 });
 
 app.listen(port, () => {
