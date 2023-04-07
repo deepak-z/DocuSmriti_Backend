50c897b54a5e6f18579a49a0e17cce3356ade26a
Arshad Khan <arshad.k@zebpay.com>
2023-04-07 16:55:48 +0530

    improvements

--- helper/userHelper.js
+++ helper/userHelper.js
@@ -20,11 +20,15 @@ export async function linkUserWallet(req) {
     if(err != null){
         return ["Unable to find user", err]
     }
+    const userEmail = user.email;
     [user, err] = await getUserByWalletAddress(req.body.walletAddress)
     if(err != null){
         return ["Unable to find user", err]
     }
     if(user != null){
+        if(userEmail == user.email){
+            return [{"message":"Wallet already linked"}, null]
+        }
         return ["Wallet linked to another user", "INVALID WALLET LINKED"]
     }
     [user, err] = await addWallet(req)
--- prisma/migrations/20230407110217_init/migration.sql
+++ prisma/migrations/20230407110217_init/migration.sql
@@ -2,9 +2,11 @@
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "wallet_address" TEXT NOT NULL,
+    "wallet_address" TEXT NOT NULL DEFAULT '',
     "email" TEXT NOT NULL,
     "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
