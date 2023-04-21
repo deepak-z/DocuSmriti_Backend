ea736fc24f962eb5deea04828b748abad1528a91
Deepak Kumar <108132126+deepak-z@users.noreply.github.com>
2023-04-21 16:21:20 +0530

    Interacting with contract (#12)
    * BasicSetupAdded
    
    * MoreFiles
    
    * Updated.ENVFile
    
    * backend_contract
    
    ---------
    
    Co-authored-by: Arshad Khan <arshad.k@zebpay.com>

--- config/config.js
+++ config/config.js
@@ -1,28 +1,35 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 const config = {
   db: {
-    port: process.env.postgresPort,
-    host: process.env.postgresHost,
-    user: process.env.postgresUser,
+    port:     process.env.postgresPort,
+    host:     process.env.postgresHost,
+    user:     process.env.postgresUser,
     password: process.env.postgresPassword,
     database: process.env.postgresDatabase,
   },
   email: {
-    service: process.env.emailService,
-    from: process.env.emailFrom,
+    service:  process.env.emailService,
+    from:     process.env.emailFrom,
     password: process.env.emailPassword,
   },
   server: {
     host: process.env.host,
-    port: process.env.port
+    port: process.env.port,
   },
   corsOptions: {
-    origin: '*',
-    credentials: true, 
+    origin: "*",
+    credentials: true,
     optionSuccessStatus: 200,
-  }
-}
+  },
+  blockchain: {
+    rpcUrl:                       process.env.rpcUrl,
+    alchemyApiKey:                process.env.alchemyApiKey,
+    contractAddress:              process.env.contractAddress,
+    docuSmritiWalletAddress:      process.env.docuSmritiWalletAddress,
+    docuSmritiWalletPrivateKey:   process.env.docuSmritiWalletPrivateKey
+  },
+};
 
-export default config
+export default config;
--- routes/serviceRouter.js
+++ routes/serviceRouter.js
@@ -1,13 +1,16 @@
 import { Router } from "express"
 const router = Router()
 
-import { logIn } from "../controller/userController.js"
+import { LogIn } from "../controller/userController.js"
 import { verifyGoogleToken, verifyUser } from "../utility/middleware.js"
+import { AddContract } from "../controller/contractController.js"
 
 router.get("/", (req, res) => {
     res.status(200).send("Welcome to DocuSmriti Backend")
 })
 
-router.post("/logIn", verifyGoogleToken, logIn)
+router.post("/logIn", verifyGoogleToken, LogIn)
+
+router.post("/addContract", verifyGoogleToken, verifyUser(true, false), AddContract)
 
 export default router
