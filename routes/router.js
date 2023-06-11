a3c4d2872950cff0844160c478f703c1b83a9da9
Deepak Kumar <108132126+deepak-z@users.noreply.github.com>
2023-06-11 16:54:59 +0530

    cleaned (#28)

--- routes/router.js
+++ routes/router.js
@@ -1,24 +1,32 @@
 import { Router } from "express"
 const router = Router()
 
-import { LogIn, SaveUserKycInfo, GetKycInfo } from "../controller/userController.js"
 import { verifyGoogleToken, verifyUser } from "../utility/middleware.js"
-import { AddContract, ApproveTransaction } from "../controller/contractController.js"
-import { EstimateAcceptContractGasPrice, EstimateAddContractGasPrice } from "../controller/contractController.js"
 
+import { LogIn, SaveUserKycInfo, GetKycInfo } from "../controller/userController.js"
+import { AddContract, ApproveTransaction, EstimateAcceptContractGasPrice, EstimateAddContractGasPrice } from "../controller/contractController.js"
+import { sendEmail } from "../controller/emailController.js"
+
+// Service Routes
 router.get("/", (req, res) => {
     res.status(200).json({'message' : "Welcome to DocuSmriti Backend. Up and Running"})
 })
 
+// Email Routes
+router.post("/mail",verifyGoogleToken,verifyUser(true,true), sendEmail)
+
+// User Routes
 router.post("/logIn", verifyGoogleToken, LogIn)
 
+// Kyc Routes
+router.get("/kyc",verifyGoogleToken,verifyUser(true,true), GetKycInfo)
+router.post("/kyc", verifyGoogleToken, verifyUser(true, false), SaveUserKycInfo)
+router.post("selfie", verifyGoogleToken, verifyUser(true, false), SaveUserKycInfo)
+
+// Contract Routes
 router.post("/addContract", verifyGoogleToken, verifyUser(true, true), AddContract)
 router.get("/addContract/getQuote", verifyGoogleToken, verifyUser(true, true), EstimateAddContractGasPrice)
 router.post("/acceptContract", verifyGoogleToken, verifyUser(true, true), ApproveTransaction)
 router.get("/acceptContract/getQuote", verifyGoogleToken, verifyUser(true, true), EstimateAcceptContractGasPrice)
-
-router.get("/kyc",verifyGoogleToken,verifyUser(true,true), GetKycInfo)
-router.post("/kyc", verifyGoogleToken, verifyUser(true, false), SaveUserKycInfo)
-router.post("selfie", verifyGoogleToken, verifyUser(true, false), SaveUserKycInfo)
 
 export default router
